9984c7c6ac0f54704857d698a0530c755283faed
Pedro Moreira <pedrocmoreira97@gmail.com>
2023-11-12 10:03:55 -0300

    fix: correções em erros da página

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,21 @@
 import 'package:app_despesas/models/transaction.dart';
 import 'package:flutter/material.dart';
 
-main() => runApp(const ExpensesApp());
+void main() => runApp(const ExpensesApp());
 
 class ExpensesApp extends StatelessWidget {
   const ExpensesApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+  MyHomePage({super.key});
 
   final _transactions = [
     Transaction(
@@ -35,23 +35,28 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text('Desppesas pessoais')),
-        body: const Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            SizedBox(
-              width: double.infinity,
-              child: Card(
-                color: Colors.blue,
-                elevation: 5,
-                child: Text('Gráfico'),
-              ),
+      appBar: AppBar(title: const Text('Despesas pessoais')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          const SizedBox(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              elevation: 5,
+              child: Text('Gráfico'),
             ),
-            Card(
-              child: Text('Lista de transações'),
-            )
-          ],
-        ));
+          ),
+          Column(
+            children: _transactions.map((tr) {
+              return Card(
+                child: Text(tr.title ?? 'Título não disponível'),
+              );
+            }).toList(), // Adicione o método `toList()` para converter o iterable em uma lista.
+          ),
+        ],
+      ),
+    );
   }
 }
